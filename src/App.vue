773db7a8536531930254243393baeee389aa0283
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-05-08 23:23:35 -0300

    feat: Primeiro posicionamento dos elementos da tela

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Meu carrinho de compras V2</div>
+  <div class="pagina">
+    <h1 class="titulo">Carrinho de compras</h1>
+    <main class="conteudo">
+      <!-- <Produtos /> -->
+      <!-- <Resumo /> -->
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.pagina {
+  display: flex;
+  justify-content: center;
+  padding: 0 30px 30px;
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+}
+
+.titulo {
+  font-size: 32px;
+  padding: 30px 0;
+}
+</style>
